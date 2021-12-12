f824fb0a82f592a552506b00ff5e17dea93929d9
David <davidaase@hotmail.com>
2021-12-12 21:44:26 +0100

    fix scrollspy bug

--- js/_partials/scrolling.js
+++ js/_partials/scrolling.js
@@ -14,7 +14,7 @@
         selector: '#spy-nav ul a', // Default link selector
         selectorHeader: fixedHeaderSelector, // Fixed header selector
         container: window, // The element to spy on scrolling in (must be a valid DOM Node)
-        offset: 48, // Distance in pixels to offset calculations
+        offset: 128, // Distance in pixels to offset calculations
         activeClass: 'active', // Class to apply to active navigation link and its parent list item
         scrollDelay: false, // Wait until scrolling has stopped before updating the navigation
         callback: function (nav) {
