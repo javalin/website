fc2f938190f1034b74934e94fa2a2072644c84a7
David <davidaase@hotmail.com>
2021-12-12 17:49:29 +0100

    improve scrolling

--- js/_partials/main.js
+++ js/_partials/main.js
@@ -1,36 +1,28 @@
 // add target blank to all external links
-for (var i = 0; i < document.links.length; i++) {
+for (let i = 0; i < document.links.length; i++) {
     if (document.links[i].hostname !== window.location.hostname) {
         document.links[i].target = "_blank";
         document.links[i].rel = "noopener";
     }
 }
 
-var offset = 24;
-var fixedHeaderSelector = 'body > header';
-smoothScroll.init({ // https://github.com/cferdinandi/smooth-scroll
-    selector: 'a[href^="#"]', // Selector for links (must be a class, ID, data attribute, or element tag)
-    selectorHeader: fixedHeaderSelector, // Selector for fixed headers [optional]
-    speed: 500, // Integer. How fast to complete the scroll in milliseconds
-    easing: 'easeInOutCubic', // Easing pattern to use
-    offset: offset, // Integer. How far to offset the scrolling anchor location in pixels
-    callback: function (anchor, toggle) {
-    } // Function to run after scrolling
-});
-
-// scroll to current element on load
-if (window.location.hash && performance.navigation.type !== 1) {
-    setTimeout(function () {
-        window.scrollTo(0, window.scrollY - 106);
-    }, 0);
-}
-
-setTimeout(function () {
+(() => {
+    let fixedHeaderSelector = '.top-nav-header';
+    smoothScroll.init({ // https://github.com/cferdinandi/smooth-scroll
+        selector: 'a[href^="#"]', // Selector for links (must be a class, ID, data attribute, or element tag)
+        selectorHeader: fixedHeaderSelector, // Selector for fixed headers [optional]
+        speed: 300, // Integer. How fast to complete the scroll in milliseconds
+        easing: 'easeInOutCubic', // Easing pattern to use
+        offset: 24, // Integer. How far to offset the scrolling anchor location in pixels
+        callback: function (anchor, toggle) {
+            // Function to run after scrolling
+        }
+    });
     gumshoe.init({ // https://github.com/cferdinandi/gumshoe (scrollspy)
         selector: '#spy-nav ul a', // Default link selector
         selectorHeader: fixedHeaderSelector, // Fixed header selector
         container: window, // The element to spy on scrolling in (must be a valid DOM Node)
-        offset: offset, // Distance in pixels to offset calculations
+        offset: 48, // Distance in pixels to offset calculations
         activeClass: 'active', // Class to apply to active navigation link and its parent list item
         scrollDelay: false, // Wait until scrolling has stopped before updating the navigation
         callback: function (nav) {
@@ -40,7 +32,8 @@ setTimeout(function () {
             }
         }
     });
-}, 500);
+})();
+
 
 if (document.location.pathname.includes("/documentation")) {
     // "Added in" labels
